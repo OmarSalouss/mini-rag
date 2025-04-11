d62faa5db6917bf1db0801df2638b2f1dae88dfe
git config user.name <omar.salous2@gmail.com>
2025-04-11 22:33:22 +0300

    Add file upload functionality with validation and settings integration

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Depends
 import os
+from helpers.config import get_settings, Settings
 
 base_router = APIRouter(
     prefix="/api/v1",
@@ -7,9 +8,11 @@ base_router = APIRouter(
 )
 
 @base_router.get("/")
-async def welcome():
-    app_name = os.getenv('APP_NAME')
-    app_version = os.getenv('APP_VERSION')
+async def welcome(app_settings: Settings = Depends(get_settings)): 
+    # Depends(get_settings): Ensure to import get_settings from your config module and already exist, this make fastapi more efficient and faster
+
+    app_name = app_settings.APP_NAME
+    app_version = app_settings.APP_VERSION
     
     return {
         "app_name": app_name,
